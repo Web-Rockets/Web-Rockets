9240c3fc7b77b277a614261158111343b05373a2
Nak Young Kim <nydkim@gmail.com>
2020-06-25 21:29:52 -0400

    initial set up

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const server = app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
+const socket = require('socket.io');
+const io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {}
+
+app.use('/asset', express.static(path.join(__dirname, '../client/asset')));
