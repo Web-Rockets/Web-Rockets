13e906f77b5efc31bd893f54baa948637c53dc1c
Kevin <ruan8111@gmail.com>
2020-06-27 13:29:11 -0400

    worked on login and render canvas when succesfullly logged in w/ react router

--- client/App.jsx
+++ client/App.jsx
@@ -7,17 +7,56 @@ import Canvas from './component/logedIn/canvas.jsx';
 import Home from './component/logedIn/home'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logStatus: false
+    }
+    this.onLogged = this.onLogged.bind(this);
+  }
+
+  onLogged(username, password) {
+    console.log('onLogged invoked')
+    console.log(username, password)
+    fetch('/login', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ username, password })
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('onLogged data:', data);
+      this.setState({
+        ... this.state,
+        logStatus: data.logStatus
+      })
+    })
+    .catch(err => console.log('err onLogged:', err))
+  }
+
   render() {
+    let renderCanvas;
+    if (this.state.logStatus) {
+      renderCanvas = <Canvas />
+    }
+    let renderLogin;
+    if (!this.state.logStatus) {
+      renderLogin = <Route path="/login" render={(routeProps) => (
+        <Login onLogged={ this.onLogged }/>
+      )}/>
+    }
     return (
       <Router>
         <div>
           <NavBar/>
           <Switch>
-            <Route path="/login"  component={Login}/>
-            <Route path="/canvas"  component={Canvas}/>
-            <Route path="/home"  component={Home}/>
+            {/* <Route path="/login"  component={Login} /> */}
+            { renderLogin }
+            {/* <Route path="/canvas"  component={Canvas}/> */}
+            <Route path="/"  component={Home}/>
           </Switch>
         </div>
+        { renderCanvas }
     </Router>
     )
   }
