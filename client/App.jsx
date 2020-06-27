5d0e9bb2db3ddf3eb5f12314520c23fe349fa499
ebnouhamoud <ebnouhamoud@gmail.com>
2020-06-27 11:00:04 -0400

    hello

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
-// import NavBar from './component/navigationBar/navBar.js';
+import NavBar from './component/navigationBar/navBar.js';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Login from './component/logedIn/Login.js';
+import Login from './component/logedIn/Login';
 import Canvas from './component/logedIn/canvas.jsx';
 import Home from './component/logedIn/home'
 
