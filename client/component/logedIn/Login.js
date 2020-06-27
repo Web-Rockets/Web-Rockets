1b28ccc3ad0016564ed45846ba888540f5600e9c
exquizzle <40224753+exquizzle@users.noreply.github.com>
2020-06-27 13:42:27 -0400

    merged

--- client/component/logedIn/Login.js
+++ client/component/logedIn/Login.js
@@ -19,19 +19,11 @@ class Login extends Component {
   render() {
     return(
       <div className='form'>
-<<<<<<< HEAD
-        <form method='POST' action='/signup'>
-          <input name="username" type="text" placeholder="username"></input>
-          <input name="password" type="password" placeholder="password"></input>
-          <input type='submit' value='Login' />  
-        </form>
-=======
         {/* <form> */}
           <input name="username" type="text" placeholder="username" id="username"></input>
           <input name="password" type="password" id="password"></input>
           <input type='button' value='Log In' id="button"/>  
         {/* </form> */}
->>>>>>> 131b37281f22aec978f3ca9f8587ccec3a35574e
         <a href='./signup'>Sign up</a>
 
       </div>
